947147f9f0406a1ed6ecab363a570447a880225c
Indrajeet <indrajeetseru@gmail.com>
2021-05-30 16:34:35 +0530

    Added Coustom Exception MoodAnalyser UC3

--- src/moodanalyser/MoodAnalyser.java
+++ src/moodanalyser/MoodAnalyser.java
@@ -3,21 +3,23 @@ package moodanalyser;
 public class MoodAnalyser {
 	private final String message;
 
-	public MoodAnalyser(String message) {
-		this.message = message;
-	}
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to the Mood Analyser");
-	}
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Mood Analyser");
+    }
 
-	public String analyse() {
-		try {
-			if (message.contains("Sad"))
-				return "SAD";
-			return "HAPPY";
-		} catch (NullPointerException e) {
-			return "HAPPY";
-		}
-	}
+    public String analyse() throws MoodAnalyserException {
+        try {
+            if (message.length() == 0)
+                throw new MoodAnalyserException("Plz enter the mood can not empty", MoodAnalyserException.UserDefineDataType.EMPTY_EXCEPTION);
+            if (message.contains("Sad"))
+                return "SAD";
+            return "HAPPY";
+        } catch (NullPointerException e) {
+            throw new MoodAnalyserException("Plz enter the mood can not null", MoodAnalyserException.UserDefineDataType.NULL_EXCEPTION);
+        }
+    }
 }
---
+++ src/moodanalyser/MoodAnalyserException.java
@@ -0,0 +1,19 @@
+package moodanalyser;
+
+public class MoodAnalyserException extends Throwable {
+    public UserDefineDataType userDefinedObject;
+
+    public MoodAnalyserException(String message, UserDefineDataType userDefinedObject) {
+        super(message);
+        this.userDefinedObject = userDefinedObject;
+    }
+
+    public enum UserDefineDataType {
+        NULL_EXCEPTION,
+        EMPTY_EXCEPTION,
+        NO_SUCH_CLASS,
+        NO_SUCH_METHOD,
+        NO_SUCH_FIELD,
+        FIELD_INVOCATION_ISSUE;
+    };
+}
